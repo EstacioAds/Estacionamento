8be6daec8689b11848b3f6e51df921a2a74778dd
Francila Weidt <fran.weidt@gmail.com>
2020-09-10 20:57:06 -0300

    Add files via upload

---
+++ estacionamento/Carro.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package estacionamento;
+
+/**
+ *
+ * @author franw
+ */
+public class Carro {
+    private String placa;
+    private String modelo;
+    private String cor;
+    
+    public void setPlaca(String placa){
+        this.placa = placa;
+    }
+    public String getPlaca(){
+       return this.placa;
+    }
+    
+}
